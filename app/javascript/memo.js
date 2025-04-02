82cf701b27e0f03f0e52d75fbf456a125c16f0fa
d-magaki <dkmk.03112600@gmail.com>
2025-04-02 15:35:24 +0900

    コントローラーにリクエストを送信する

---
+++ app/javascript/memo.js
@@ -0,0 +1,11 @@
+function post (){
+  const form = document.getElementById("form");
+  form.addEventListener("submit", () => {
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+  });
+};
+
+window.addEventListener('turbo:load', post);
